4c7147d7653563fffc2fb9a260c77e8826a732e5
LUZ P JIMENEZ <146458622+LuzJ123@users.noreply.github.com>
2025-05-09 14:44:32 -0400

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,3 +1,9 @@
+/**
+ * Calculates the nth term in the Fibonacci sequence using recursion.
+ *
+ * @param n the position in the Fibonacci sequence
+ * @return the nth Fibonacci number
+ */
 public class Fibonacci {
     public static int fibonacci(int n) {
         if (n <= 1) return n;
